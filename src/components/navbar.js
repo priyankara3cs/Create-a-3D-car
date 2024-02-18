29e9b099926b096237068123bd71e906e0ff493b
priyankara3cs <priyankara@3cs.solutions>
2024-02-18 08:52:27 +0530

    update the navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <div className="logo">3D Car</div>
-        <ul className="nav-links">
+        <div className="burger-menu" onClick={toggleMenu}>
+          ☰
+        </div>
+        <ul className={`nav-links ${showMenu ? 'show' : ''}`}>
           <li><a href="/">Home</a></li>
           <li><a href="/about/">About</a></li>
           <li><a href="/services/">Services</a></li>
